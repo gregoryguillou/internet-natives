84523f3278b7f76ba15993589dec8854cf8cebf0
Jean-Sébastien Ney <jeansebastien.ney@gmail.com>
2022-11-13 16:30:57 +0100

    use modifiers

--- evm/contracts/Lock.sol
+++ evm/contracts/Lock.sol
@@ -20,15 +20,22 @@ contract Lock {
         owner = payable(msg.sender);
     }
 
-    function withdraw() public {
+    // Create a modifier that the function can be execulted only after unlockTime
+    modifier isUnlocked() {
+        require(block.timestamp >= unlockTime, "You can't withdraw yet");
+        _;
+    }
+
+    // Create a modifier that the msg.sender must be the owner modifier
+    modifier onlyOwner() {
+        require(msg.sender == owner, "You aren't the owner");
+        _;
+    }
+
+    function withdraw() public onlyOwner isUnlocked {
         // Uncomment this line, and the import of "hardhat/console.sol", to print a log in your terminal
         // console.log("Unlock time is %o and block timestamp is %o", unlockTime, block.timestamp);
-
-        require(block.timestamp >= unlockTime, "You can't withdraw yet");
-        require(msg.sender == owner, "You aren't the owner");
-
         emit Withdrawal(address(this).balance, block.timestamp);
-
         owner.transfer(address(this).balance);
     }
 }
